03ae6d4a04fd3b40ef6d0f930b007c4fe5e52aed
David <dsi28@hotmail.com>
2020-09-12 11:59:39 -0500

    set up routes/router for promotions and partners

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+//Set routers:
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouters = require('./routes/promotionRouter');
+const partnerRouters = require('./routes/partnerRouter');
+
 const hostname = 'localhost';
 const port = 3000;
 
@@ -9,48 +14,9 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/campsites', (req,res,next)=>{
-    res.statusCode=200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-});
-
-app.get('/campsites',(req,res)=>{
-    res.end('Will send campsites');
-});
-
-app.post('/campsites',(req,res)=>{
-    res.end(`will add campsite with name: ${req.body.name} and description ${req.body.description}`);
-});
-
-app.put('/campsites', (req,res)=>{
-    res.statusCode = 403;
-    res.end(`PUT operation not supported on /campsites`);
-});
-
-app.delete('/campsites', (req,res)=>{
-    res.end(`deleting all campsites`);
-});
-
-
-app.get('/campsites/:campsiteId',(req,res)=>{
-    res.end(`Will send details of campsite id : ${req.params.campsiteId}`);
-});
-
-app.post('/campsites/:campsiteId',(req,res)=>{
-    req.statusCode=403;
-    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
-});
-
-app.put('/campsites/:campsiteId', (req,res)=>{
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`wil update the campsite ${req.body.name} 
-        with the description ${req.body.description}`);
-});
-
-app.delete('/campsites/:campsiteId', (req,res)=>{
-    res.end(`deleting campsite ${req.params.campsiteId}`);
-});
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouters);
+app.use('/partners', partnerRouters);
 
 app.use(express.static(__dirname+'/public'));
 
